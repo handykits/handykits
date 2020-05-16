bd2b9f1de77da82c1c2f4eaaea3f9a3d4ffd96a8
Jerick Gutierrez <45832168+handykits@users.noreply.github.com>
2020-05-16 16:30:29 -0700

    Update base64EncodeDecode.js

--- dev/base64EncodeDecode.js
+++ dev/base64EncodeDecode.js
@@ -1,1 +1,44 @@
-//encode string to b64
+/* Base 64 Encode and Decode String */
+function getInput(){
+    let input = document.getElementById("toolInput").nodeValue;
+    return input;
+}
+
+function updateOutput(output){
+    document.getElementById(toolOutput).value = output;
+}
+
+/*ENCODE*/
+
+jQuery("#encode").click(function(){
+    console.log("Attempting to encode string");
+    try{
+        let output = base64Encode(getInput());
+        updateOutput(output);
+        console.log("Encode Successful");
+    }catch(error){
+        console.error.error;
+    }
+});
+
+function base64Encode(input){
+    let output = btoa(input);
+    return output;
+}
+
+/*DECODE */
+jQuery("#devode").click(function(){
+    console.log("Attempting to decode string");
+    try{
+        let output = base64Decode(getInput());
+        updateOutput(output);
+        console.log("Decode Successful");
+    }catch(error){
+        console.error.error;
+    }
+});
+
+function base64Decode(input){
+    let output = atob(input);
+    return output;
+}
